5775c94ca2b5ee81430ecab7358ea3c3c4b4e3cb
rster2002 <istargamesspam+github@gmail.com>
2017-08-17 20:49:18 +0200

    Mirror a.0.2

--- dev/projects/mirror/list.js
+++ dev/projects/mirror/list.js
@@ -1,1 +1,2 @@
+version("a.0.2","snapshot",["+ added beacons","+ added beacon traveling","+ added more planets","+ added spawning dialog","+ added founa module","+ added first package of founa","~ fixed bug with structure spawning","~ fixed bug with spawning","~ fixed some other minor bugs"]);
 version("a.0.1","snapshot",["First version"]);
