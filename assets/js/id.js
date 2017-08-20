650fb576c17fb75a24eaccecf03e4b4430312eb4
rster2002 <istargamesspam+github@gmail.com>
2017-08-20 10:22:05 +0200

    Id

--- assets/js/id.js
+++ assets/js/id.js
@@ -1,3 +1,17 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDCgnh6ezKcNkcpAUtGXuiN77jxlDbLPck",
+	authDomain: "dewebsite-bae27.firebaseapp.com",
+	databaseURL: "https://dewebsite-bae27.firebaseio.com",
+	projectId: "dewebsite-bae27",
+	storageBucket: "dewebsite-bae27.appspot.com",
+	messagingSenderId: "437303961105"
+};
+firebase.initializeApp(config);
+
+database = firebase.database();
+dbRef = database.ref("ids");
+
 var urlParam = function(name, w){
 	w = w || window;
 	var rx = new RegExp('[\&|\?]'+name+'=([^\&\#]+)'),
@@ -5,21 +19,37 @@ var urlParam = function(name, w){
 	return !val ? '':val[1];
 }
 
-function requireId(type) {
-	urlId = urlParam("id");
-	if (localStorage.getItem("id")) {
-		return localStorage.getItem("id");
-	} else {
-		if (urlId === null || urlId === '') {
-			alert("The id is empty");
+urlId = urlParam("id");
+
+if (urlId === "") {
+	alert("There is no id in url or registrated");
+	location.href="index.html";
+}
+
+if (localStorage.getItem("id")) {
+	urlId = localStorage.getItem("id");
+}
+
+function requireId() {
+	dbRef.child(urlId).once("value").then(function (snapshot){
+		dbIdState = snapshot.val();
+		if (dbIdState === null || dbIdState.valid === "false") {
+			alert("This id is invalid (anymore)");
 			location.href="index.html";
-		} else {
-			if (confirm("Do you want to register this id? You can't change it later!") === true) {
+		}
+		if (dbIdState.registrated === "false") {
+			if (confirm("Do you want to register this id on this device? You can't use it on a other device!") === true) {
+				dbRef.child(urlId).child("registrated").set("true");
 				localStorage.setItem("id",urlId);
-				return urlId;
+				re = true;
 			} else {
 				location.href="index.html";
 			}
 		}
-	}
+		if (dbIdState.valid === "true") {
+			document.getElementById("mainStory").setAttribute("style","display:block");
+		} else {
+			reValid = false;
+		}
+	});
 }
