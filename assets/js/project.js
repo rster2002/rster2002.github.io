29b811d3369c2474e33887063fcc8905bf688c9e
rster2002 <istargamesspam+github@gmail.com>
2017-08-22 18:09:59 +0200

    versions

---
+++ assets/js/project.js
@@ -0,0 +1,14 @@
+function openPrj(name, header) {
+	sessionStorage.removeItem("prjName");
+	sessionStorage.removeItem("header");
+	sessionStorage.setItem("prjName",name);
+	if (header === false) {
+		sessionStorage.setItem("header","false");
+	} else {
+		sessionStorage.setItem("header",header);
+	}
+	location.href="dev/project.html"
+}
+
+function loadPrj() {
+}
--- assets/js/versions.js
+++ assets/js/versions.js
@@ -1,6 +1,7 @@
 height = 300;
 versionNr = 0;
 projectName = sessionStorage.getItem("pro_name");
+mode = false;
 
 function version(name, type, changes) {
 	versionNr += 1;
@@ -33,6 +34,22 @@ function version(name, type, changes) {
 		var element = document.createElement("h1");
 		element.innerHTML = "snapshot " + name;
 		document.getElementById(versionNr).appendChild(element);
+		
+		var element = document.createElement("h1");
+		element.innerHTML = "snapshot " + name;
+		element.setAttribute("id","snapshot");
+		document.getElementById("allChanges").appendChild(element);
+		
+		var element = document.createElement("ul");
+		element.setAttribute("id","allChanges" + versionNr);
+		document.getElementById("allChanges").appendChild(element);
+		
+		var arrayLength = changes.length;
+		for (var i = 0; i < arrayLength; i++) {
+			var element = document.createElement("li");
+			element.innerHTML = changes[i];
+			document.getElementById("allChanges" + versionNr).appendChild(element);
+		}
 	}
 	
 	if (type === "pre") {
@@ -46,6 +63,24 @@ function version(name, type, changes) {
 		var element = document.createElement("h1");
 		element.innerHTML = "pre-release " + name;
 		document.getElementById(versionNr).appendChild(element);
+		
+		var element = document.createElement("h1");
+		element.innerHTML = "pre-release " + name;
+		element.setAttribute("id","pre");
+		document.getElementById("allChanges").appendChild(element);
+		
+		var element = document.createElement("ul");
+		element.setAttribute("id","allChanges" + versionNr);
+		document.getElementById("allChanges").appendChild(element);
+		
+		var arrayLength = changes.length;
+		for (var i = 0; i < arrayLength; i++) {
+			var element = document.createElement("li");
+			element.innerHTML = changes[i];
+			document.getElementById("allChanges" + versionNr).appendChild(element);
+		}
+		
+		
 	}
 	
 	if (type === "release") {
@@ -59,6 +94,22 @@ function version(name, type, changes) {
 		var element = document.createElement("h1");
 		element.innerHTML = "release " + name;
 		document.getElementById(versionNr).appendChild(element);
+		
+		var element = document.createElement("h1");
+		element.innerHTML = "release " + name;
+		element.setAttribute("id","release");
+		document.getElementById("allChanges").appendChild(element);
+		
+		var element = document.createElement("ul");
+		element.setAttribute("id","allChanges" + versionNr);
+		document.getElementById("allChanges").appendChild(element);
+		
+		var arrayLength = changes.length;
+		for (var i = 0; i < arrayLength; i++) {
+			var element = document.createElement("li");
+			element.innerHTML = changes[i];
+			document.getElementById("allChanges" + versionNr).appendChild(element);
+		}
 	}
 }
 
@@ -68,9 +119,25 @@ function openPage(name, changes) {
 	for (var i = 0; i < arrayLength; i++) {
 		changesList += "<li>" + changes[i] + "<li>";
 	}
-	var fileName = "'versions/" + projectName + "%20" + name + ".zip'";
+	var fileName = "'projects/versions/" + projectName + "%20" + name + ".zip'";
 	var opened = window.open('');
-	opened.document.write("<html><head><title>" + projectName + " " + name + "</title><link rel='icon' href='../../../assets/logo_black.png'><style>.header h1 {background-image: " + color + "}</style><link rel='stylesheet' media='screen and (min-device-width: 800px)'href='../../../assets/css/changes.css'><link rel='stylesheet' media='screen and (max-device-width: 800px)' href='../../../assets/css/changes.m.css'></head><body><div class='header'><h1>" + projectName + " " + name + "</h1></div><div class='changes'><h1>changes</h1><ul id='changes'>" + changesList + "</ul><button onclick=location.href=" + fileName + ">download</button></div></body></html>");
+	opened.document.write("<html><head><title>" + projectName + " " + name + "</title><link rel='icon' href='../assets/logo_black.png'><style>.header h1 {background-image: " + color + "}</style><link rel='stylesheet' media='screen and (min-device-width: 800px)'href='../assets/css/changes.css'><link rel='stylesheet' media='screen and (max-device-width: 800px)' href='../assets/css/changes.m.css'></head><body><div class='header'><h1>" + projectName + " " + name + "</h1></div><div class='changes'><h1>changes</h1><ul id='changes'>" + changesList + "</ul><button onclick=location.href=" + fileName + ">download</button></div></body></html>");
+}
+
+function change() {
+	if (mode === false) {
+		document.getElementById("allChanges").setAttribute("style","display:block;");
+		document.getElementById("versions").setAttribute("style","display:none;");
+		mode = true;
+		return;
+	}
+	
+	if (mode === true) {
+		document.getElementById("versions").setAttribute("style","display:block;");
+		document.getElementById("allChanges").setAttribute("style","display:none;");
+		mode = false;
+		return;
+	}
 }
 //localStorage.setItem('changes',"a.1");
 //changes = ["text","text2"];
