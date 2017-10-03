727fbf86e2a959635041839c135006739f2234b3
Bjørn <istargamesspam+github@gmail.com>
2017-10-03 09:44:44 +0200

    Neus

--- dev/projects/minetoon.js
+++ dev/projects/minetoon.js
@@ -1,5 +1,5 @@
 version("a.0.5", "snapshot", ["+ Added splatzones","~ Fixed some bugs"]);
-version("a.0.4", "snapshot", ["* Scores are now shown as precentages","~ Bug with splattershot when shooting over a edge fixed"]);
+version("a.0.4", "snapshot", ["* Scores are now shown as precentages","~ Bug with splattershot when shooting over an edge fixed"]);
 version("a.0.3", "snapshot", ["* fixed some bugs"]);
 version("a.0.2", "snapshot", ["* bug fixes", "+ added some bugs", "~ known bug where purple team splattershot doesn't work","~ known bug that makes every map act strange exept urchin underpass"]);
 version("a.0.1", "snapshot", ["* first version"]);
