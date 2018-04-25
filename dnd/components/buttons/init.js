7dc1a25c74fb136b19c2ad8d8ba6a96079016447
rster2002 <istargamesspam+github@gmail.com>
2018-04-25 13:55:09 +0200

    VA0.11

--- dnd/components/buttons/init.js
+++ dnd/components/buttons/init.js
@@ -1,16 +1,31 @@
 $("button.wave--button").each(function(index){
-	var iconFront = $(this).attr("icon-left");
-	if (iconFront !== undefined) {
-		$(this).prepend("<i class='material-icons wave--right'>" + iconFront + "</i>")
-	}
-	
-	var iconBack = $(this).attr("icon-right");
-	if (iconBack !== undefined) {
-		$(this).append("<i class='material-icons wave--left'>" + iconBack + "</i>")
-	}
-	
-	var icon = $(this).attr("icon");
-	if (iconBack === undefined && iconFront === undefined && icon !== undefined) {
-		$(this).append("<i class='material-icons wave--center'>" + icon + "</i>")
-	}
+	setupButton(this);
 });
+
+function setupButton(here) {
+	if (!$(here).hasClass("setup")) {
+		var iconFront = $(here).attr("icon-left");
+		if (iconFront !== undefined) {
+			$(here).prepend("<i class='material-icons wave--right'>" + iconFront + "</i>")
+		}
+
+		var iconBack = $(here).attr("icon-right");
+		if (iconBack !== undefined) {
+			$(here).append("<i class='material-icons wave--left'>" + iconBack + "</i>")
+		}
+
+		var icon = $(here).attr("icon");
+		if (iconBack === undefined && iconFront === undefined && icon !== undefined) {
+			$(here).append("<i class='material-icons wave--center'>" + icon + "</i>")
+		}
+		
+		$(here).addClass("setup");
+	}
+}
+
+(function( $ ){
+   $.fn.button = function() {
+	   setupButton(this);
+	   return this;
+   }; 
+})( jQuery );
