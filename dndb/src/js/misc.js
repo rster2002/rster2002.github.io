0935e64e576d11015e5dbe68aecfa4524d5c9dcb
rster2002 <istargamesspam+github@gmail.com>
2018-11-09 19:43:57 +0100

    D&D vB1.13 ~ Bug fixes
    MTG vb0.1

--- dndb/src/js/misc.js
+++ dndb/src/js/misc.js
@@ -1,5 +1,5 @@
 global = {
-	version: "vB1.8"
+	version: "vB1.13"
 }
 
 var url = document.URL;
@@ -24,13 +24,13 @@ if (DEV) {
 }
 
 if (!DEV) {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-	ga('create', 'UA-102147810-1', 'auto');
-	ga('send', 'pageview');
+	gtag('config', 'UA-102147810-1', {
+		'page_title' : 'D&D Web app'
+	});
 }
 
 log = console.log;
@@ -207,7 +207,7 @@ a = {
 			// } else {
 			// 	ga('send', 'event', "dnd-" + global.version, action, label, `Uid: ${uid}: ` + value);
 			// }
-			ga('send', 'event', "dnd-" + global.version, action, label, `Uid: ${uid}: ` + value);
+			gtag('send', 'event', "dnd-" + global.version, action, label, `Uid: ${uid}: ` + value);
 		}
 	}
 }
