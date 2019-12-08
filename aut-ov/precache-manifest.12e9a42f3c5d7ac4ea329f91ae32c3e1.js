585b24fd94edbce0f1f27b340abd1faddc929e2b
Bjørn Reemer <bjornsivar@gmail.com>
2019-12-08 22:32:12 +0100

    AUT-OV vb1.1
    + Added routing
    + Revamped the whole UI

---
+++ aut-ov/precache-manifest.12e9a42f3c5d7ac4ea329f91ae32c3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a6bcdc8873ca8b7e3652feacb4e5f63",
+    "url": "/aut-ov/stylesheet.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/aut-ov/robots.txt"
+  },
+  {
+    "revision": "ad54b5db34ec476dfe83",
+    "url": "/aut-ov/js/chunk-vendors.d343bf4b.js"
+  },
+  {
+    "revision": "f6117d552acd8f3d7711",
+    "url": "/aut-ov/js/app.d4eff99e.js"
+  },
+  {
+    "revision": "27074a120340aa96ef68aaad846547a4",
+    "url": "/aut-ov/index.html"
+  },
+  {
+    "revision": "f6117d552acd8f3d7711",
+    "url": "/aut-ov/css/app.e1f69c39.css"
+  }
+];
